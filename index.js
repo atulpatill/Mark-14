e270b51ae852468ab78689178810be2b08e6ea5b
Atul Patil <atulpatil539@gmail.com>
2021-08-21 19:01:11 +0530

    feat:added

--- index.js
+++ index.js
@@ -3,31 +3,38 @@ const stocksQuantity = document.querySelector("#stocks-quantity")
 const currentPrice = document.querySelector("#current-price")
 const checkBtn = document.querySelector("#check-btn")
 const outputBox = document.querySelector("#output-box")
-function clickHandler(){
-    const ip = Number(initialPrice.value) ;
-    const qty = Number (stocksQuantity.value);
+
+
+function clickHandler() {
+    const ip = Number(initialPrice.value);
+    const qty = Number(stocksQuantity.value);
     const crr = Number(currentPrice.value);
-    calculateProfitAndLoss(ip,qty,crr)
+    calculateProfitAndLoss(ip, qty, crr)
 }
 
-function calculateProfitAndLoss(initial,quantity,current){
-    if(initial > current){
+function calculateProfitAndLoss(initial, quantity, current) {
+    if (initial > current) {
         // code for loss
-        var loss = (initial-current) * quantity;
-        var lossPercentage = (loss/initial) * 100;
-      outputBox.innerText ='hey the loss is ' + loss + ' and the percent is ' + lossPercentage + '%'
-    }else if(current > initial){
+        var loss = (initial - current) * quantity;
+        var lossPercentage = (loss / initial) * 100;
+        outputBox.innerText = 'Ohh!! the loss is ' + loss + ' and the percent is ' + lossPercentage + '%'
+        outputBox.style.color = "Red"
+    } else if (current > initial) {
         // code for profit
-        var profit = (current-initial) * quantity;
-        var profitPercentage = (profit/current) * 100;
-        outputBox.innerText ='Hurray!! Your Profit is ' + profit + ' and the percent is ' + profitPercentage +'%'
-        
-    }else{
+        var profit = (current - initial) * quantity;
+        var profitPercentage = (profit / current) * 100;
+        outputBox.innerText = 'Hurray!! Your Profit is ' + profit + ' and the percent is ' + profitPercentage + '%'
+        outputBox.style.color = "Green"
+
+    } else {
         // the rest of code
-        outputBox.innerText ='Unfortunetly No Profit And fortuneatly No Loss!'
+        outputBox.innerText = 'Fortuneatly No Loss!! Unfortunetly No Profit !!'
+        outputBox.style.color = "purple"
     }
+
 }
-calculateProfitAndLoss(10,5,10)
+
+
 
 
 checkBtn.addEventListener("click", clickHandler)
