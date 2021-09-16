eab31b84b07b05a674120dd8ed1a5cb9db288a54
Atul Patil <atulpatil539@gmail.com>
2021-09-16 12:08:04 +0530

    fix: app taking negative inputs

--- index.js
+++ index.js
@@ -9,7 +9,15 @@ function clickHandler() {
     const ip = Number(initialPrice.value);
     const qty = Number(stocksQuantity.value);
     const crr = Number(currentPrice.value);
-    calculateProfitAndLoss(ip, qty, crr)
+    if(ip === 0 | qty === 0 | crr === 0) {
+        outputBox.innerText = "Enter all the inputs";
+        outputBox.style.color = "red"
+        outputBox.style.fontSize ="25px";
+      } else {
+        calculateProfitAndLoss(ip, qty, crr)
+      }
+   
+
 }
 
 function calculateProfitAndLoss(initial, quantity, current) {
@@ -17,7 +25,7 @@ function calculateProfitAndLoss(initial, quantity, current) {
         // code for loss
         var loss = (initial - current) * quantity;
         var lossPercentage = (loss / initial) * 100;
-        outputBox.innerText = 'Ohh!! the loss is ' + loss + ' and the percent is ' + lossPercentage + '%'
+        outputBox.innerText = 'Ohh!! the loss is ' + loss + ' and the percent is ' + lossPercentage.toFixed(2) + '%'
         outputBox.style.color = "pink"
         outputBox.style.fontSize ="25px";
       
@@ -25,7 +33,7 @@ function calculateProfitAndLoss(initial, quantity, current) {
         // code for profit
         var profit = (current - initial) * quantity;
         var profitPercentage = (profit / current) * 100;
-        outputBox.innerText = 'Hurray!! Your Profit is ' + profit + ' and the percent is ' + profitPercentage + '%'
+        outputBox.innerText = 'Hurray!! Your Profit is ' + profit + ' and the percent is ' + profitPercentage.toFixed(2) + '%'
         outputBox.style.color = "Green"
         outputBox.style.fontSize ="25px";
 
